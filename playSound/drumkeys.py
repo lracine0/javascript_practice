1c751238758ad7d5a2ef8ce537ba3a203961c5f0
vagrant <vagrant@vagrant.vm>
2020-04-21 17:51:31 +0000

    initial setup complete

---
+++ playSound/drumkeys.py
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>JS Drum Kit</title>
+  <link rel="stylesheet" href="static/drum_style.css">
+</head>
+<body>
+
+
+  <div class="keys">
+    <div data-key="65" class="key">
+      <kbd>A</kbd>
+      <span class="sound">clap</span>
+    </div>
+    <div data-key="83" class="key">
+      <kbd>S</kbd>
+      <span class="sound">hihat</span>
+    </div>
+    <div data-key="68" class="key">
+      <kbd>D</kbd>
+      <span class="sound">kick</span>
+    </div>
+    
+
+  <audio data-key="65" src="sounds/app,ause2_x.wav"></audio>
+  <audio data-key="83" src="sounds/bloop_x.wav"></audio>
+  <audio data-key="68" src="sounds/cymbals.wav"></audio>
+ 
+
+<script>
+
+</script>
+
+
+</body>
+</html>
